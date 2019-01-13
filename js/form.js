0a1606e656c68a7857118f2d9689f393de7aafeb
AYastrebtsov <yastrebsov@icloud.com>
2019-01-14 01:33:39 +0300

    еще поработал над критериями

--- js/form.js
+++ js/form.js
@@ -1,18 +1,18 @@
 var map_opener = document.querySelector(".modal");
 var main_button = document.querySelector(".element1__index");
 
-  map_opener.classList.add("modal-js");
+map_opener.classList.add("modal-js");
 
-  main_button.addEventListener("click", function (evt) {
+main_button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  map_opener.classList.toggle("modal-js");
+});
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (map_opener.classList.contains("modal")) {
       evt.preventDefault();
-      map_opener.classList.toggle("modal-js");
-    });
-
-  document.addEventListener("keydown", function (evt) {
-      if (evt.keyCode === 27) {
-        if (map_opener.classList.contains("modal")) {
-          evt.preventDefault();
-          map_opener.classList.add("modal-js");
-        }
-      }
-    });
+      map_opener.classList.add("modal-js");
+    }
+  }
+});
